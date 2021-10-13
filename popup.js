b6cf5f2978a826aa3cd3897c5c97abf6c080ff3d
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2021-10-13 21:25:07 +0400

    feat: new changes

--- popup.js
+++ popup.js
@@ -1,17 +1,24 @@
-const button = document.querySelector(".button")
-const circle = document.querySelector(".circle")
-
-let buttonOn = false;
-
-button.addEventListener("click", ()=>{
-    if(!buttonOn){
-        buttonOn = true;
-        circle.style.animation = "moveCircleRight 2s forwards";
-        button.style.animation = "backgroundRed 2s forwards";
-    }
-    else{
-        buttonOn = false;
-        circle.style.animation = "moveCircleLeft 2s forwards";
-        button.style.animation = "backgroundGreen 2s forwards";
-    }
-})
+if (document.querySelector(".popup")){
+    const button = document.querySelector(".button")
+    const circle = document.querySelector(".circle")
+    
+    let buttonOn = false;
+    button.addEventListener("click", ()=>{
+        if(!buttonOn){
+            buttonOn = true;
+            circle.style.animation = "moveCircleRight 2s forwards";
+            button.style.animation = "backgroundRed 2s forwards";
+            chrome.tabs.executeScript({
+                file: "appOn.js"
+            })
+        }
+        else{
+            buttonOn = false;
+            circle.style.animation = "moveCircleLeft 2s forwards";
+            button.style.animation = "backgroundGreen 2s forwards";
+            chrome.tabs.executeScript({
+                file: "appOff.js"
+            })
+        }
+    })
+}
